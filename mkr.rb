97b3e0e8440dee5271d1bc9ab17913609193ef90
y_uuki <yuki.tsubo@gmail.com>
2014-12-08 03:37:33 +0900

    Add mkr formula

---
+++ mkr.rb
@@ -0,0 +1,29 @@
+require 'formula'
+
+HOMEBREW_MKR_VERSION='0.1.0'
+class Mkr < Formula
+  homepage 'https://github.com/y-uuki/mkr'
+  url "https://github.com/mackerelio/mkr/releases/download/v#{HOMEBREW_MKR_VERSION}/mkr_darwin_amd64.zip"
+  sha1 'cf666ec207666353f975012a9f432b4fa0bfe419'
+
+  version HOMEBREW_MKR_VERSION
+  head 'https://github.com/y-uuki/mkr', :using => :git, :branch => 'master'
+
+  if build.head?
+    depends_on 'go' => :build
+  end
+
+  def install
+    if build.head?
+      gopath = buildpath/'.go'
+
+      ( gopath/'src/github.com/mackerelio/mkr' ).make_relative_symlink buildpath
+
+      ENV['GOPATH'] = gopath
+      system 'make build'
+
+    end
+
+    bin.install 'mkr'
+  end
+end
