8c48683f572dbe71704501f1df03ad9c68207f02
y_uuki <yuki.tsubo@gmail.com>
2015-04-27 19:08:10 +0900

    Fix sha1

--- mkr.rb
+++ mkr.rb
@@ -4,7 +4,7 @@ HOMEBREW_MKR_VERSION='0.1.0'
 class Mkr < Formula
   homepage 'https://github.com/mackerelio/mkr'
   url "https://github.com/mackerelio/mkr/releases/download/v#{HOMEBREW_MKR_VERSION}/mkr_darwin_amd64.zip"
-  sha1 'cf666ec207666353f975012a9f432b4fa0bfe419'
+  sha1 'bd73d12de6f7ff9f868c30513e19ef763072b344'
 
   version HOMEBREW_MKR_VERSION
   head 'https://github.com/mackerelio/mkr', :using => :git, :branch => 'master'
