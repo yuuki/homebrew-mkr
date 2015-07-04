53f19c3f300a02140b7ec28dfc8837d3077082de
y_uuki <yuki.tsubo@gmail.com>
2015-07-05 03:21:49 +0900

    Update to 0.3.0

--- mkr.rb
+++ mkr.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
-HOMEBREW_MKR_VERSION='0.2.0'
+HOMEBREW_MKR_VERSION='0.3.0'
 class Mkr < Formula
   homepage 'https://github.com/mackerelio/mkr'
   url "https://github.com/mackerelio/mkr/releases/download/v#{HOMEBREW_MKR_VERSION}/mkr_darwin_amd64.zip"
-  sha1 '964b8dab2bb4f19617d781b69e3829234d5e6cff'
+  sha1 '13edaa8dc69df601d29e964fd822d44b91444c9b'
 
   version HOMEBREW_MKR_VERSION
   head 'https://github.com/mackerelio/mkr', :using => :git, :branch => 'master'
